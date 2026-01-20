1ceeeee0f97a2f009df8f63220f839a755671bd9
Sana AlEdilbi <155321164+SanaAledilbi@users.noreply.github.com>
2026-01-21 00:49:25 +0300

    10_create_ticket_payments_table.sql

--- Train_Tickes_Database/schema/10_create_ticket_payments_table.sql
+++ Train_Tickes_Database/schema/10_create_ticket_payments_table.sql
@@ -2,13 +2,14 @@
 -- Stores payment transaction information
 
 CREATE TABLE TicketPayments (
-    PaymentID NUMBER PRIMARY KEY,
-    TicketID NUMBER,
-    PassengerID NUMBER,
-    passengerName VARCHAR2(100),
-    PurchasedDate TIMESTAMP,
-    PaymentMethod VARCHAR2(20),
-    PaymentAmount NUMBER(10,2),
-    -- Add foreign keys
-    CONSTRAINT fk_TicketID_TP FOREIGN KEY (TicketID) REFERENCES Ticket(TicketID)
+    PaymentID INT,
+    PassengerID INT,
+    passengerName VARCHAR(50),
+    TicketID INT,
+    PaymentAmount DECIMAL(10, 2),
+    PaymentDate TIMESTAMP,
+    PaymentMethod VARCHAR(20),
+    
+    CONSTRAINT pk_PaymentID PRIMARY KEY(PaymentID),
+    CONSTRAINT fk_passengerNamePayment FOREIGN KEY (passengerID) REFERENCES Passengers (passengerID)
 );
